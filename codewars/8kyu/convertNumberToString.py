00cee3b949f93fd6d6f453ccb99ca589bb8c576b
Christopher Jin <chrisjin1@gmail.com>
2024-10-07 12:46:31 -0400

    2 8kyu fundamentals

---
+++ codewars/8kyu/convertNumberToString.py
@@ -0,0 +1,9 @@
+def number_to_string(num):
+    return str(num)
+
+print(number_to_string(1), "1")
+print(number_to_string(123), "123")
+print(number_to_string(-100), "-100")
+print(type(number_to_string(-100)))
+
+# Pass

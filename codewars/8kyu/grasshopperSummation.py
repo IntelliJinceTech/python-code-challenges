b6c359a5a09cef774e4bfb43cf22c678aae2d24f
Christopher Jin <chrisjin1@gmail.com>
2024-10-14 20:54:52 -0400

    2+1 more

---
+++ codewars/8kyu/grasshopperSummation.py
@@ -0,0 +1,6 @@
+def summation(num):
+    return sum(range(1,num+1))
+
+
+print(summation(2)) # 3 (1+2)
+print(summation(8)) # 1+2+3+4+5+6+7+8
---
+++ codewars/8kyu/stringRepeat.py
@@ -0,0 +1,6 @@
+def repeat_str(numOfRepeat, string):
+    return numOfRepeat * string
+
+
+print(repeat_str(4, 'hello')) # hellohellohellohello
+print(repeat_str(2, 'abc')) # abcabc

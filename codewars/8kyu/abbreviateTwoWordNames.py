9a88a9a147e0236c5ffbab21e3b7a73036ff87f0
Christopher Jin <chrisjin1@gmail.com>
2024-11-18 23:58:21 -0500

    prep for abbreviate 2 word names

---
+++ codewars/8kyu/abbreviateTwoWordNames.py
@@ -0,0 +1,18 @@
+def abbrev_name(name):
+    return
+
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+#
+# The output should be two capital letters with a dot separating them.
+#
+# It should look like this:
+#
+# Sam Harris => S.H
+#
+# patrick feeney => P.F
+
+# todo split name input by the space to an array
+# todo capitalize each word
+# todo take first letter of each word and return a string with period between
+
+
